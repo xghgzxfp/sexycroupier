eab6efe0afd31083741018822e93591edf05f729
刘一铭 <ymliu6899@gmail.com>
2018-03-30 14:45:30 -0400

    rewrite match's insert and update functions

--- worldcup/model.py
+++ worldcup/model.py
@@ -68,14 +68,7 @@ class Match:
         self.weight = weight
 
     def __str__(self):
-        print('id: ', self.id)
-        print('league: ', self.league)
-        print('match_time: ', self.match_time)
-        print('handicap_display: ', self.handicap_display)
-        print(self.a)
-        print(self.b)
-        print('weight: ', self.weight)
-        return ""
+        return self.__dict__
 
 
 def generate_match_id(match_time, team_a, team_b):
@@ -99,26 +92,46 @@ def generate_handicap_pair(handicap_display):
 
 def insert_match(league_name, match_time, handicap_display, team_a, team_b, premium_a, premium_b, score_a, score_b):
     new_match = Match(league_name, match_time, handicap_display, team_a, team_b, premium_a, premium_b, score_a, score_b)
+    # do nothing if duplicate
+    if db.match.find({"id": new_match.id}).limit(1).count():
+        return
     db.match.insert(new_match.__dict__)
     return
 
 
+def update_match_score(match_time, team_a, team_b, score_a, score_b):
+    match_id = generate_match_id(match_time, team_a, team_b)
+    db.match.update(
+        {"id": match_id},
+        {"$set": {"a.score": int(score_a), "b.score": int(score_b)}}
+    )
+    return
 
-def update_match_handicap(matchid, a, b): return
+def update_match_handicap(match_time, team_a, team_b, handicap_display):
+    match_id = generate_match_id(match_time, team_a, team_b)
+    db.match.update(
+        {"id": match_id},
+        {"$set": {"handicap": generate_handicap_pair(handicap_display)}}
+    )
+    return
 
 def update_match_gamblers(matchid, team, gambler):
     """Update betting decision in database
 
     """
-
     list_out = ("a" if team == "b" else "b") + ".player"
     list_in = team + '.player'
+    return db.match.update({"id": matchid}, {"$pull": {list_out: gambler}, "$addToSet": {list_in: gambler}})
 
-    return db.match.update({"id" : matchid}, {"$pull"      : { list_out : gambler}, "$addToSet"  : { list_in : gambler}})
 
+def update_match_weight(match_time, team_a, team_b, weight):
+    match_id = generate_match_id(match_time, team_a, team_b)
+    db.match.update(
+        {"id": match_id},
+        {"$set": {"weight": float(weight)}}
+    )
+    return
 
-def update_match_score(match, a, b): return
-def update_match_weight(match, weight): return
 def find_matches(cup): return
 
 
@@ -135,4 +148,5 @@ def generate_series(cup): return
 
 
 if __name__ == "__main__":
-    insert_match('意甲', datetime(2018, 3, 31, 18, 30), '受半球/一球', '博洛尼亚', '罗马', '1.98', '1.88', '', '')
+    #insert_match('意甲', datetime(2018, 3, 31, 18, 30), '受半球/一球', '博洛尼亚', '罗马', '1.98', '1.88', '', '')
+    update_match_score(datetime(2018, 3, 31, 18, 30), '博洛尼亚', '罗马', '0', '0')
