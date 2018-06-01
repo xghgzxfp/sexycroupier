78d4bc16c125ab0f840f8d47ea6788d42670e564
John Hu <hszqqq12@gmail.com>
2018-06-01 21:10:07 +0800

    Submit bet choice using AJAX for better UX

--- worldcup/app.py
+++ worldcup/app.py
@@ -115,8 +115,8 @@ def auth_signup():
 @authenticated
 def index():
     if request.method == 'POST':
-        match_id = request.values.get('match_id')
-        new_choice = request.values.get('betchoice')
+        match_id = request.values.get('match-id')
+        new_choice = request.values.get('bet-choice')
         model.update_match_gamblers(match_id, new_choice, g.me.name)
 
     matches = model.find_matches(cup=app.config['LEAGUE_NAME'], reverse=True)
