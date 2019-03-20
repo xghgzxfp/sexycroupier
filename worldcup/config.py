67fb328d5df1f0e4d972af7e6b1e1a99355eee39
刘一铭 <ymliu6899@gmail.com>
2019-03-20 17:29:30 -0400

    Remove tournamentdb in config

--- worldcup/config.py
+++ worldcup/config.py
@@ -7,6 +7,5 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'super secret key')
 
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
 MONGO_LOGINDB = os.getenv('MONGO_LOGINDB', 'logins')
-MONGO_TOURNAMENTDB = os.getenv('MONGO_TOURNAMENTDB', 'worldcup2018')
 WECHAT_APPID = os.getenv('WECHAT_APPID', '')
 WECHAT_APPSECRET = os.getenv('WECHAT_APPSECRET', '')
