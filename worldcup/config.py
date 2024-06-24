0970c5e47107cf15bcf699c9a5545e11aa1ba986
John Hu <hszqqq12@gmail.com>
2024-06-23 22:26:07 -0700

    Add eurocup2024

--- worldcup/config.py
+++ worldcup/config.py
@@ -41,10 +41,10 @@ TOURNAMENTS = [
         league='世界杯',
         display='2018 世界杯',
         weight_schedule=[
-            (datetime.datetime(2018, 7, 5), 2),     # group games and 1/8 final
+            (datetime.datetime(2018, 7, 5), 2),     # group games + 1/8 final
             (datetime.datetime(2018, 7, 9), 4),     # 1/4 final
             (datetime.datetime(2018, 7, 13), 8),    # semi final
-            (datetime.datetime(2018, 7, 16), 16),   # final and 3rd 4th final
+            (datetime.datetime(2018, 7, 16), 16),   # final + 3rd playoff
         ],
     ),
     Tournament(
@@ -54,7 +54,7 @@ TOURNAMENTS = [
         weight_schedule=[
             (datetime.datetime(2019, 4, 19), 4),    # 1/4 final
             (datetime.datetime(2019, 5, 10), 8),    # semi final
-            (datetime.datetime(2019, 6, 3), 16),    # final and 3rd 4th final
+            (datetime.datetime(2019, 6, 3), 16),    # final + 3rd playoff
         ],
     ),
     Tournament(
@@ -62,10 +62,21 @@ TOURNAMENTS = [
         league='世界盃',
         display='2022 世界杯',
         weight_schedule=[
-            (datetime.datetime(2022, 12, 8), 2),    # group games and 1/8 final
+            (datetime.datetime(2022, 12, 8), 2),    # group games + 1/8 final
             (datetime.datetime(2022, 12, 13), 4),   # 1/4 final
             (datetime.datetime(2022, 12, 16), 8),   # semi final
-            (datetime.datetime(2022, 12, 19), 16),  # final and 3rd 4th final
+            (datetime.datetime(2022, 12, 19), 16),  # final + 3rd playoff
+        ],
+    ),
+    Tournament(
+        dbname='eurocup2024',
+        league='欧国杯',
+        display='2024 欧洲杯',
+        weight_schedule=[
+            (datetime.datetime(2024, 7, 4), 2),     # 1/8 final
+            (datetime.datetime(2024, 7, 8), 4),     # 1/4 final
+            (datetime.datetime(2024, 7, 12), 8),    # semi final
+            (datetime.datetime(2024, 7, 16), 16),   # final + 3rd playoff
         ],
     ),
 ]
